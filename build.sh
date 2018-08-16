f5e4a4ccc2137f78ba53f85a4386ab770c14485c
JohnHolmesII <jedistephen@hotmail.com>
2018-08-15 23:57:54 -0700

    Update build scripts

--- build.sh
+++ build.sh
@@ -1,12 +1,16 @@
-if [ ! -d build ]
+if [ -d build ]
 then
+	rm -rf build/
+	mkdir build
+else
 	mkdir build
 fi
 
 cd glfw/
-cmake -G "MSYS Makefiles" -DBUILD_SHARED_LIBS=ON -DLIB_SUFFIX=64 -DGLFW_BUILD_EXAMPLES=OFF -DGLFW_BUILD_TESTS=OFF -DGLFW_BUILD_DOCS=OFF GLFW_VULKAN_STATIC=OFF .;
+cmake -G "MSYS Makefiles" -DBUILD_SHARED_LIBS=ON -DLIB_SUFFIX=64 -DGLFW_BUILD_EXAMPLES=OFF -DGLFW_BUILD_TESTS=OFF -DGLFW_BUILD_DOCS=OFF GLFW_VULKAN_STATIC=OFF . &&
 make glfw && cp -f src/glfw3.dll ../build/;
 
 cd ../build;
-cmake -G "MSYS Makefiles" ../;
-make;
+cmake -G "MSYS Makefiles" ../ && make;
+
+rm -rf CMakeFiles cmake_install.cmake CMakeCache.txt Makefile;
