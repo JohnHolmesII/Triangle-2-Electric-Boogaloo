33a6f8e0034e4796d70f860681fb2a6cdb98cba2
JohnHolmesII <jedistephen@hotmail.com>
2018-08-15 23:49:25 -0700

    Update ignore; add build script for windows

---
+++ build.sh
@@ -0,0 +1,12 @@
+if [ ! -d build ]
+then
+	mkdir build
+fi
+
+cd glfw/
+cmake -G "MSYS Makefiles" -DBUILD_SHARED_LIBS=ON -DLIB_SUFFIX=64 -DGLFW_BUILD_EXAMPLES=OFF -DGLFW_BUILD_TESTS=OFF -DGLFW_BUILD_DOCS=OFF GLFW_VULKAN_STATIC=OFF .;
+make glfw && cp -f src/glfw3.dll ../build/;
+
+cd ../build;
+cmake -G "MSYS Makefiles" ../;
+make;
