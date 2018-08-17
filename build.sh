1f4ebe0549c7d383e306f692e0321ee220fc0560
JohnHolmesII <jedistephen@hotmail.com>
2018-08-16 21:26:44 -0700

    Move to submodule model

--- build.sh
+++ build.sh
@@ -6,11 +6,11 @@ else
 	mkdir build
 fi
 
-cd glfw/
+cd 3rdparty/glfw/
 cmake -G "MSYS Makefiles" -DBUILD_SHARED_LIBS=ON -DLIB_SUFFIX=64 -DGLFW_BUILD_EXAMPLES=OFF -DGLFW_BUILD_TESTS=OFF -DGLFW_BUILD_DOCS=OFF GLFW_VULKAN_STATIC=OFF . &&
-make glfw && cp -f src/glfw3.dll ../build/;
+make glfw && cp -f src/glfw3.dll ../../build/;
 
-cd ../build;
+cd ../../build;
 cmake -G "MSYS Makefiles" ../ && make;
 
 rm -rf CMakeFiles cmake_install.cmake CMakeCache.txt Makefile;
