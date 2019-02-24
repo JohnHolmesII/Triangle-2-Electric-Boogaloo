f5b259a23fd099e4d501bddd9cb7583e5e8c61ff
JohnHolmesII <jedistephen@hotmail.com>
2019-02-23 21:54:02 -0800

    Done with first page: Base Code

--- src/main.c
+++ src/main.c
@@ -1,11 +1,26 @@
+#define GLFW_INCLUDE_VULKAN
+#include <glfw3.h>
+
 #include <vulkan/vulkan.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include "types.h"
 
+#define WIDTH 800
+#define HEIGHT 600
+#define TITLE "Triangle 2: Electric Boogaloo"
+
+GLFWwindow* window = NULL;
 
 byt initWindow()
 {
+	glfwInit();
+
+	glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
+	glfwWindowHint(GLFW_RESIZABLE, GLFW_FALSE);
+
+	window = glfwCreateWindow(WIDTH, HEIGHT, TITLE, NULL, NULL);
+
 	return CELL_OK;
 }
 
@@ -16,10 +31,16 @@ byt initVulkan()
 
 void mainLoop()
 {
+	while (!glfwWindowShouldClose(window))
+	{
+		glfwPollEvents();
+	}
 }
 
 void cleanup()
 {
+	glfwDestroyWindow(window);
+	glfwTerminate();
 }
 
 int main()
